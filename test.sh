8b58accaf0102b57f4d779550181c4e8dfa2dc73
ryan5500 <ryan5500@gmail.com>
2021-05-28 10:41:53 +0900

    add test case

--- test.sh
+++ test.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 postgres_ver=("10-alpine" "10" "11-alpine" "11" "12-alpine" "12" "13-alpine" "13")
-base_port=5432
-test_query="select 'a' < 'A'"
+test_queries=("select 'a' < 'A'" "select 'a' = 'A'" "select 'A' = 'A'")
 
-for e in ${postgres_ver[@]}; do
-    echo "Postgresql ver: ${e} result"
-    PGPASSWORD=secret psql -U postgres -h 0.0.0.0 -p ${base_port} postgres -c "${test_query}"
+for ((i = 0; i < ${#test_queries[@]}; i++)); do
+    query="${test_queries[$i]}"
+    echo "# test_query: ${query}"
+
+    base_port=5432
+    for ver in ${postgres_ver[@]}; do
+        echo "Postgresql ver: ${ver} result"
+        PGPASSWORD=secret psql -U postgres -h 0.0.0.0 -p ${base_port} postgres -c "${query}"
+        echo ""
+
+        let base_port++
+    done
     echo ""
-
-    let base_port++
 done
